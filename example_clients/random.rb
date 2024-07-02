752d33db0b9cc395bcc9b66c2015436e99586a72
Michael Yin <michael@gloryleague.com>
2024-07-03 11:19:14 +1200

    example_clients/random.rb: improve

--- example_clients/random.rb
+++ example_clients/random.rb
@@ -1,19 +1,19 @@
 # frozen_string_literal: true
 
-url = 'http://localhost:3000/players/1.json'
+API_URL = 'http://localhost:3000/players/1.json'
 
 loop do
   sleep 0.1
-  data = HTTParty.get(url).parsed_response
+  data = HTTParty.get(API_URL).parsed_response
   waiting_for_command = data.fetch('waiting_for_command')
   next unless waiting_for_command
 
   state = data.fetch('state')
   puts "state: #{state.inspect}"
-  command = %w[north south east west shoot move].sample
+  command = %w[north south east west shoot shoot move move move move move move].sample
   puts "command: #{command}"
   HTTParty.put(
-    url,
+    API_URL,
     headers: { 'Content-Type' => 'application/json' },
     body: {  player: { command:, waiting_for_command: false } }.to_json
   )
